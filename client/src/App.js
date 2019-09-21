f04b88744891d62c5f32c276c3cbd06e537040c0
Tiffany Marroquin <tiffany.marroq@gmail.com>
2019-09-21 15:44:29 -0400

    adding in data files

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
+import HomeData from './Components/HomeData';
 
 import "./App.css";
 
@@ -50,21 +51,15 @@ class App extends Component {
 
   render() {
     if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
+      return (
+        <div>
+          HOME HERO.
+          <HomeData />
+        </div>);
+    } 
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>HOME HERO</h1>
       </div>
     );
   }
