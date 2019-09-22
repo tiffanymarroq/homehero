74bad9732830f9f587b31a8e77dc9cfe8f895922
Tiffany Marroquin <tiffany.marroq@gmail.com>
2019-09-22 01:06:29 -0400

    adding in graphs

--- client/src/Components/UserData/UserData.js
+++ client/src/Components/UserData/UserData.js
@@ -9,36 +9,44 @@ class HomeData extends Component {
             user: {
                 history: {
                     personal: {
-                        washingMachine : {
-                            600 : 6,
-                            100 : 10,
-                        },
-                        dishwasher : {
-                            800 : 15,
-                            1800 : 20,
-                        },
                         bathroom : {
                             800 : 6,
                             1200: 30,
                             1500 : 8,
                             18: 40
                         },
-                    },
-                    'commmunity' : {
                         washingMachine : {
                             600 : 6,
-                            100 : 15,
+                            1300 : 10,
+                            1700: 10
                         },
                         dishwasher : {
-                            800 : 10,
-                            1800 : 18,
+                            800 : 15,
+                            1200 : 13,
+                            1800 : 20,
                         },
+                       
+                    },
+                    'commmunity' : {
                         bathroom : {
                             800 : 6,
                             1200: 20,
                             1500 : 8,
                             18: 25
                         },
+                        washingMachine : {
+                            600 : 6,
+                            1300 : 15,
+                            1700: 30
+
+                        },
+                        dishwasher : {
+                            800 : 10,
+                            1200 : 13,
+
+                            1800 : 18,
+                        },
+                       
                     } 
                 }
             }
@@ -50,12 +58,32 @@ class HomeData extends Component {
     render(){
         let waterCard = Object.keys(this.state.user.history.personal).map( (data,i) => {
             let title = data;
+            let labels = []
+            let waterData = Object.keys(this.state.user.history.personal[data]).map( waterData => {
+                labels.push(waterData)
+                return this.state.user.history.personal[data][waterData]
+            })
+            console.log(waterData)
+            console.log(labels)
             if(data === 'washingMachine'){
                 title = 'Washing Machine'
             }
             return (
                 <div className="water-card">
                     <p className="bold card-title">{title}</p>
+                    <Line
+                       data = {{
+                            
+                            datasets: [{
+                                data: waterData,
+                                label: 'Water Used (gallon)',
+                                borderColor: ['#4A0080'],
+                                backgroundColor: ['rgba(74,0,128,.2)']
+                            }],
+                            labels: labels
+                        }}
+                     
+                        />
                    
                 </div> 
             )
@@ -63,17 +91,7 @@ class HomeData extends Component {
         return(
             <div>
                 {waterCard}
-                {/* <Line
-                        data={ [{
-                            x: 10,
-                            y: 20
-                        }, {
-                            x: 15,
-                            y: 10
-                        }]}
-                        width={100}
-                        height={50}
-                        options={{ maintainAspectRatio: false }}  /> */}
+     
             </div>
         )
     }
