d75e59381bdc65da9d09d0116012c78cb247bd4f
Tiffany Marroquin <tiffany.marroq@gmail.com>
2019-09-22 00:28:14 -0400

    setting up rewards

---
+++ client/src/Components/Incentives/Incentives.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Button} from 'reactstrap';
+
+const Incentives = (props) => {
+
+    return (
+        <div className='rewards'>
+            <p>You've earned 40 points!</p>
+            <div className="water-card">
+            <p className="bold">Tide pods</p>
+            IMAGE
+            <Button>Claim it!</Button>
+            </div>
+
+        </div>
+    )
+}
+export default Incentives;
