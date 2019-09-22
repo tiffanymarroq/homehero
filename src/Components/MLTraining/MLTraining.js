acd8ac03a53e1808540794ed7b3956bc656cbb05
CodeSlip <jason@rawlings.io>
2019-09-21 21:00:11 -0400

    logos and things

--- src/Components/MLTraining/MLTraining.js
+++ src/Components/MLTraining/MLTraining.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import {Button} from 'reactstrap';
+import TimeAgo from 'react-timeago';
 
-const mltraining=()=> {
+const mltraining=(props)=> {
+    let time = props.timeStamp;
+
     return <div>
-            It's ML Training time
+            <h2>Activity Detected</h2>
+            <br/>
+            <p>Hi Tiffany, did you run the wachine machine on <TimeAgo date={time}/>?</p>
+            <Button>
+                <div>
+                    <img src='/Assets/washing-machine.png'/>
+                    Yes
+                </div>
+            </Button>
+            <Button>
+                <div>
+                    <img src=''/>
+                    No
+                </div>
+            </Button> 
            </div>
     }
 
