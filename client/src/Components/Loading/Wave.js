0a7e8cf39ea6dd17e437278f6234c703d399143b
Tiffany Marroquin <tiffany.marroq@gmail.com>
2019-09-21 22:41:24 -0400

    fixed logo

--- client/src/Components/Loading/Wave.js
+++ client/src/Components/Loading/Wave.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './wave.css';
 
 const wave =() => {
-    return <div>
+    return (<div>
                 <img className="logo-image" src='/Assets/logo.png'/>
                 <div className="ocean">
                     <div className="wave"></div>
                     <div className="wave"></div>
                 </div>
-           </div>
+           </div>)
     }
 
 export default wave;
